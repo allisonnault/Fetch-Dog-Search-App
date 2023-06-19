f34d0080b102a33781c72bbd1913adebbb9bfe5d
allisonnault <allisonrnault@gmail.com>
2023-06-19 11:35:15 -0500

    removed zipcodes

--- fetch-dog-search/src/components/Search.js
+++ fetch-dog-search/src/components/Search.js
@@ -17,8 +17,6 @@ const Search = () => {
   const [next, setNext] = useState("");
   const [prev, setPrev] = useState("");
   const [favorites, setFavorites] = useState(new Set());
-  const [zipCodes, setZipCodes] = useState("");
-  const [zipArray, setZipArray] = useState([]);
   const options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
   useEffect(() => {
