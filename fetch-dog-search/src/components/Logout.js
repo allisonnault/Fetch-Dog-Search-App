c441e286da888c67fbdaff6055a56100360aa9af
allisonnault <allisonrnault@gmail.com>
2023-06-19 16:36:59 -0500

    still tweaking

--- fetch-dog-search/src/components/Logout.js
+++ fetch-dog-search/src/components/Logout.js
@@ -21,7 +21,7 @@ const Logout = () => {
                     "Content-Type": "application/json"
                 },
         });
-        window.location.href = "/";
+        window.location.href = "#/";
     }
 };
 
